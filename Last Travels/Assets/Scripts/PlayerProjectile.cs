e24a4271584fd2887636966a4d5392899e5411c8
centuri0 <jt.wayfarer@gmail.com>
2014-11-11 16:45:28 -0800

    Reeeeeally crappy projectile effect added. No damage yet.

---
+++ Last Travels/Assets/Scripts/PlayerProjectile.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerProjectile : MonoBehaviour {
+
+	public Rigidbody projectile;
+	float attackSpeed = 0.5f;
+	float coolDown;
+	
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if (Time.time >= coolDown) 
+		{
+			if (Input.GetKey (KeyCode.Space)) 
+			{
+				Fire ();
+			}
+		}
+	
+	}
+
+	void Fire()
+	{
+		Rigidbody proj = Instantiate (projectile, transform.position, Quaternion.identity) as Rigidbody;
+		proj.rigidbody.AddForce (transform.up * 500);
+		coolDown = Time.time + attackSpeed;
+}
+}
