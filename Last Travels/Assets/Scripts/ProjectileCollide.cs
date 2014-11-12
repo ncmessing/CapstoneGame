7aa81b64ec245870bc8821e94b4b6302e4801b31
centuri0 <jt.wayfarer@gmail.com>
2014-11-11 18:30:22 -0800

    Added a script to projectile to destroy objects(zombies). Currently not working as intended.

---
+++ Last Travels/Assets/Scripts/ProjectileCollide.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileCollide : MonoBehaviour {
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Enemy")
+			Destroy (other.collider2D.transform.parent.gameObject);
+	}
+}
---
+++ Last Travels/Assets/Scripts/ZombieHealth.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieHealth : MonoBehaviour {
+
+
+	public int HP = 1;
+	public bool isEnemy = true;
+
+	public void Damage(int damageCount)
+	{
+				HP -= damageCount;
+		if (HP <= 0) 
+		{
+			Destroy (gameObject);
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D otherCollider)
+	{
+
+				ProjectileCollide shot = otherCollider.gameObject.GetComponent<ProjectileCollide> ();
+				if (shot != null) {
+
+								Destroy (shot.gameObject);
+						
+				}
+		}
+
+
+}
